012435c77743f0348fb15e06390369ad36a77353
Nam Tran <nam.th@review-ty.com>
2020-08-03 15:38:33 +0700

    implement dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,18 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Hi, I'm Nam. Let take a look at my repo: <Link href="/posts/first-post"><a>Link to repo</a></Link></p>
+                <p>Hi !!!</p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+                </p>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
