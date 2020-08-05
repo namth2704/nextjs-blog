4163b4b9ba6cd8261ef82f013ddd0a28d54a63c5
Nam Tran <nam.th@review-ty.com>
2020-08-05 13:55:10 +0700

    add apollo client and graphql

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import { getSortedPostsData } from '../libs/posts'
 import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
